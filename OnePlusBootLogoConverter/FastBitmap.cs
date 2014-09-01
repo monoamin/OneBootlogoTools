254f373ca0c10dea15a87ba546a8cbeb6267e025
Paul Pignet <precision1998@gmail.com>
2014-09-01 15:28:18 +0200

    Initial Commit

---
+++ OnePlusBootLogoConverter/FastBitmap.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace OneBootlogoTools
+{
+    unsafe public class FastBitmap
+    {
+        private struct PixelData
+        {
+            public byte blue;
+            public byte green;
+            public byte red;
+            public byte alpha;
+
+            public override string ToString()
+            {
+                return "(" + alpha + ", " + red + ", " + green + ", " + blue + ")";
+            }
+        }
+
+        private Bitmap workingBitmap = null;
+        private int _width = 0;
+        private BitmapData _bitmapData = null;
+        private Byte* _pBase = null;
+
+        public FastBitmap(Bitmap inputBitmap)
+        {
+            workingBitmap = inputBitmap;
+        }
+
+        public void LockImage()
+        {
+            Rectangle bounds = new Rectangle(Point.Empty, workingBitmap.Size);
+
+            _width = (int)(bounds.Width * sizeof(PixelData));
+            if (_width % 4 != 0) _width = 4 * (_width / 4 + 1);
+
+            //Lock Image
+            _bitmapData = workingBitmap.LockBits(bounds, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            _pBase = (Byte*)_bitmapData.Scan0.ToPointer();
+        }
+
+        private PixelData* pixelData = null;
+
+        public Color GetPixel(int x, int y)
+        {
+            pixelData = (PixelData*)(_pBase + y * _width + x * sizeof(PixelData));
+            return Color.FromArgb(pixelData->alpha, pixelData->red, pixelData->green, pixelData->blue);
+        }
+
+        public Color GetPixelNext()
+        {
+            pixelData++;
+            return Color.FromArgb(pixelData->alpha, pixelData->red, pixelData->green, pixelData->blue);
+        }
+
+        public void SetPixel(int x, int y, Color color)
+        {
+            PixelData* data = (PixelData*)(_pBase + y * _width + x * sizeof(PixelData));
+            data->alpha = color.A;
+            data->red = color.R;
+            data->green = color.G;
+            data->blue = color.B;
+        }
+
+        public void UnlockImage()
+        {
+            workingBitmap.UnlockBits(_bitmapData);
+            _bitmapData = null;
+            _pBase = null;
+        }
+    }
+}
